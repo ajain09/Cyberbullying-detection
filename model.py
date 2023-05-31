a60e0e070695a9568007f83d7782faec35105482
ajain09 <asthajain0901@gmail.com>
2023-05-31 22:53:08 +0530

    new commit

--- model.py
+++ model.py
@@ -19,27 +19,28 @@ import warnings
 warnings.filterwarnings('ignore')
 
 df = pd.read_csv(
-    "https://raw.githubusercontent.com/ajain09/MachineLearning/main/Datasets/train_E6oV3lV.csv")
-hate_tweet = df[df.label == 1]
-normal_tweet = df[df.label == 0]
+    "public\FinalBalancedDataset.csv")
+
+hate_tweet = df[df.Toxicity == 1]
+normal_tweet = df[df.Toxicity == 0]
 
 
 def process_tweet(tweet):
     return " ".join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])", " ", tweet.lower()).split())
 
 
-df['processed_tweets'] = df['tweet'].apply(process_tweet)
-cnt_non_fraud = df[df['label'] == 0]['processed_tweets'].count()
-df_class_fraud = df[df['label'] == 1]
-df_class_nonfraud = df[df['label'] == 0]
+df['tweets'] = df['tweet'].apply(process_tweet)
+cnt_non_fraud = df[df['Toxicity'] == 0]['tweets'].count()
+df_class_fraud = df[df['Toxicity'] == 1]
+df_class_nonfraud = df[df['Toxicity'] == 0]
 df_class_fraud_oversample = df_class_fraud.sample(cnt_non_fraud, replace=True)
 df_oversampled = pd.concat(
     [df_class_nonfraud, df_class_fraud_oversample], axis=0)
-X = df_oversampled['processed_tweets']
-y = df_oversampled['label']
+X = df_oversampled['tweets']
+y = df_oversampled['Toxicity']
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, stratify=None)
+    X, y, test_size=0.3, stratify=None)
 
 pipeline = Pipeline(steps=[('tfidf', TfidfVectorizer()),
                            ('model', SVC(C=10,
